757e28321d6d814320563f3f727e63d67df637b9
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-20 14:50:30 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// let p = document.querySelector(".pa");
+// console.log(pa.getAttribute(".pa"));
+// let value  = div.getAttribute("box");
+// console.log(value);
+let pa = document.querySelector(".pa");
+pa.setAttribute("pa" , "Para")
+console.log(pa);
+
