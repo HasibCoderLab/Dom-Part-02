b68d0bf297c2a8be531a955497214f05158a8691
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-20 22:06:55 +0600

    update JS

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // let p = document.querySelector(".pa");
-// console.log(pa.getAttribute(".pa"));
+// // console.log(pa.getAttribute(".pa"));
 // let value  = div.getAttribute("box");
 // console.log(value);
 //  =========== Style  ========  
-// let div = document.querySelector("div");
-// div.style.backgroundColor = "green" ;
-// div.style.fontSize = "25px";
-// div.style.height = "80px";
-// div.style.width = "80px"; 
+let div = document.querySelector("");//("div")
+div.style.backgroundColor = "green" ;
+div.style.fontSize = "25px";
+div.style.height = "280px";
+div.style.width = "280px"; 
 // Value Change
-// div.innerText = "Hello"
-// let pa = document.querySelector(".pa");
-// pa.setAttribute("pa" , "Para");
-// console.log(pa);
+div.innerText = "Hello"
+let pa = document.querySelector(".pa");
+pa.setAttribute("pa" , "Para");
+console.log(pa);
+
+//  ================= Create Element ===========
+// ========    /* Add btn to lasr */  =============    ==============
+let newBtn  = document.createElement("button");
+newBtn.innerText = "Click Me";
+console.log(newBtn);
+let addingDiv = document.querySelector("div");
+addingDiv.append(newBtn)
+
+
 
 
