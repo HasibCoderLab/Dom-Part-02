e95e7baa26a4c95bc56282145ae2bab98f79d955
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-20 22:09:35 +0600

    create a JS file

--- script.js
+++ script.js
@@ -2,18 +2,6 @@
 // // console.log(pa.getAttribute(".pa"));
 // let value  = div.getAttribute("box");
 // console.log(value);
-//  =========== Style  ========  
-let div = document.querySelector("");//("div")
-div.style.backgroundColor = "green" ;
-div.style.fontSize = "25px";
-div.style.height = "280px";
-div.style.width = "280px"; 
-// Value Change
-div.innerText = "Hello"
-let pa = document.querySelector(".pa");
-pa.setAttribute("pa" , "Para");
-console.log(pa);
-
 //  ================= Create Element ===========
 // ========    /* Add btn to lasr */  =============    ==============
 let newBtn  = document.createElement("button");
