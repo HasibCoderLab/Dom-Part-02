f65745de675f216fa573283e0ccb39da08b9bcb0
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-20 21:59:47 +0600

    update

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
 // let value  = div.getAttribute("box");
 // console.log(value);
 //  =========== Style  ========  
-let div = document.querySelector("div");
-div.style.backgroundColor = "green" ;
-div.style.fontSize = "25px";
-div.style.height = "80px";
-div.style.width = "80px"; 
+// let div = document.querySelector("div");
+// div.style.backgroundColor = "green" ;
+// div.style.fontSize = "25px";
+// div.style.height = "80px";
+// div.style.width = "80px"; 
 // Value Change
-div.innerText = "Hello"
-let pa = document.querySelector(".pa");
-pa.setAttribute("pa" , "Para");
-console.log(pa);
+// div.innerText = "Hello"
+// let pa = document.querySelector(".pa");
+// pa.setAttribute("pa" , "Para");
+// console.log(pa);
 
 
