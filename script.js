1f27adfcceffb3285c517fecc43851beae702113
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-20 22:21:53 +0600

    learn  create Element

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 // ========    /* Add btn to fast */  =============    ==============
 let newBtn1st = document.createElement("button");
 newBtn1st.innerText = "Click Me 1st";
-console.log(newBtn1st);
+// console.log(newBtn1st);
 let divAdd1st = document.querySelector("div");
 divAdd1st.prepend(newBtn1st)
 
@@ -27,6 +27,20 @@ newBtn.innerText = "Click Me";
 // console.log(newBtn);
 let addingDiv = document.querySelector("div");
 addingDiv.append(newBtn)
+
+// ========    /* Add btn to After node ist */  =============    ==============
+let afterNode = document.createElement("button");
+afterNode.innerText = "Click";
+// console.log(afterNode);
+let divNode = document.querySelector("div");
+divNode.after(afterNode)
+
+// ========    /* Add btn to After node ist */  =============    ==============
+let beforeNode = document.createElement("button");
+beforeNode.innerText = "Click me";
+console.log(beforeNode);
+let divNode1st = document.querySelector("div");
+divNode1st.before(beforeNode)
 
 
 
