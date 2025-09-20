ba2c2947822036824d3511878294c3a8154b3db7
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-20 15:10:40 +0600

    learning change style  with JS

--- script.js
+++ script.js
@@ -2,7 +2,16 @@
 // console.log(pa.getAttribute(".pa"));
 // let value  = div.getAttribute("box");
 // console.log(value);
+//  =========== Style  ========  
+let div = document.querySelector("div");
+div.style.backgroundColor = "green" ;
+div.style.fontSize = "25px";
+div.style.height = "80px";
+div.style.width = "80px"; 
+// Value Change
+div.innerText = "Hello"
 let pa = document.querySelector(".pa");
-pa.setAttribute("pa" , "Para")
+pa.setAttribute("pa" , "Para");
 console.log(pa);
+
 
